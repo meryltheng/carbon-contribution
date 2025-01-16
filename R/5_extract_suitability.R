732b163d406bf7af3fe2b956c9bdb1f3ffcc64a3
emmajhudgins <emma.hudgins@mail.mcgill.ca>
2025-01-16 16:49:38 +1100

    derived data upload

--- R/5_extract_suitability.R
+++ R/5_extract_suitability.R
@@ -7,28 +7,24 @@ nvis<-rast('Data/Spatial/NVIS_MVG_RAS_AA.tif')
 bioclim<-worldclim_country('bio', country="AUS",res=0.5, path='Data/Spatial/')
 bioclim<-project(bioclim, crs(nvis))
 bioclim<-crop(bioclim,nvis)
-saveRDS(bioclim, 'Data/Spatial/bioclim.rds')
-
 
 tmax<-worldclim_country('tmax',country="AUS",res=0.5, path='Data/Spatial/')
 tmax<-project(tmax, crs(nvis))
 tmax<-crop(tmax,nvis)
-saveRDS(tmax, 'Data/Spatial/tmax.rds')
 
 tmin<-worldclim_country('tmin',country="AUS",res=0.5, path='Data/Spatial/')
 tmin<-project(tmin, crs(nvis))
 tmin<-crop(tmin,nvis)
-saveRDS(tmin, 'Data/Spatial/tmin.rds')
 
 prec<-worldclim_country('prec',country="AUS",res=0.5, path='Data/Spatial/')
 prec<-project(prec, crs(nvis))
 prec<-crop(prec,nvis)
-saveRDS(prec, 'Data/Spatial/prec.rds')
-
+writeRaster(prec, 'Data/Spatial/prec.tif', overwrite=TRUE)
+  
 vapr<-worldclim_global(var="vapr",res=0.5, path='Data/Spatial/')
 vapr<-crop(vapr,project(nvis, crs(vapr)))
 vapr<-project(vapr, crs(nvis))
-saveRDS(vapr, 'Data/Spatial/vapr.rds')
+writeRaster(vapr, 'Data/Spatial/vapr.tif', overwrite=TRUE)
 
 
 ## Stolen from BIOVARS package
@@ -62,12 +58,16 @@ wetqrt <- cbind(1:nrow(prec), as.integer(apply(wet, 1, which.max)))
 tmpmin<-as.matrix(tmin)
 tmpmin <- t(apply(tmpmin, 1, window))/3 
 tminwet<- tmpmin[wetqrt]
-bioclim$tminwet<-tminminwet
+bioclim$tminwet<-tminwet
+
 #  Max Temperature of Wettest Quarter 
 tmpmax<-as.matrix(tmax)
 tmpmax <- t(apply(tmpmax, 1, window))/3
 tmaxwet<- tmpmax[wetqrt]
 bioclim$tmaxwet<-tmaxwet
+writeRaster(bioclim, 'Data/Spatial/bioclim_newvars.tif', overwrite=TRUE)
+
+bioclim<-rast('Data/Spatial/bioclim_newvars.tif')
 
 # Min  Min Temperature of Wettest Quarter 
 tmpmin2<-as.matrix(tmin)
@@ -85,4 +85,7 @@ writeRaster(bioclim, 'Data/Spatial/bioclim_newvars.tif', overwrite=TRUE)
 ##subset solar radiation to correct quarter
 
 solar<-Rast('Data/Spatial/srad.tif')
-solar$cell<-over
+solar2<-resample(solar, bioclim)
+bioclim$solar<-solar2[wetqrt]
+
+writeRaster(bioclim, 'Data/Spatial/bioclim_newvars.tif', overwrite=TRUE)
